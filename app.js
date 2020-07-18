85f04f016d91bdc2f2e63193f5685d47eba78639
Dan Sedano <dandano2006@gmail.com>
2020-07-18 12:24:20 -0700

    fished lab, need to add more styling

--- app.js
+++ app.js
@@ -11,14 +11,19 @@ const ACCESS_KEY = process.env.ACCESS_KEY;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-
 //routes
+/**
+ * Root Route
+ */
 app.get("/", async function (req, res) {
   const imageCount = 1;
   let imageUrlArray = await getRandomImage("", imageCount);
   res.render("index", { imageUrlArray: imageUrlArray });
 });
 
+/**
+ * Search Route
+ */
 app.get("/search", async function (req, res) {
   const imageCount = 9;
   let keyword = "";
@@ -31,13 +36,20 @@ app.get("/search", async function (req, res) {
   res.render("results", { imageUrlArray: imageUrlArray });
 });
 
+/**
+ * Update Favorites
+ */
 app.get("/api/updateFavorites", function (req, res) {
   let sql;
   let sqlParams;
+  let keyword;
   switch (req.query.action) {
     case "add":
       sql = "INSERT INTO favorites (imageUrl, keyword) VALUES (?,?)";
-      sqlParams = [req.query.imageUrl, req.query.keyword];
+      // if a search without keyword is performed "random" is assigned to keyword
+      keyword = req.query.keyword == "" ? "random" : req.query.keyword;
+      // console.log(keyword);
+      sqlParams = [req.query.imageUrl, keyword];
       break;
     case "delete":
       sql = "DELETE FROM favorites WHERE imageUrl = ?";
@@ -46,10 +58,37 @@ app.get("/api/updateFavorites", function (req, res) {
   } //switch
   pool.query(sql, sqlParams, function (err, rows, fields) {
     if (err) throw err;
-    console.log(rows);
+    // console.log(rows);
     res.send(rows.affectedRows.toString());
   });
 }); //api/updateFavorites
+
+/**
+ * Get Keywords
+ */
+app.get("/getKeywords", async function (req, res) {
+  let sql = "SELECT DISTINCT keyword FROM favorites ORDER BY keyword";
+  let imageUrlArray = await getRandomImage("random", 1);
+  pool.query(sql, function (err, rows, fields) {
+    if (err) throw err;
+    // console.log(rows[1]);
+    // console.log(typeof rows);
+    res.render("favorites", { imageUrlArray: imageUrlArray, rows: rows });
+  });
+}); //getKeywords
+
+/**
+ * Api getFavorites
+ */
+app.get("/api/getFavorites", function (req, res) {
+  let sql = "SELECT imageURL FROM favorites WHERE keyword = ?";
+  let sqlParams = [req.query.keyword];
+  pool.query(sql, sqlParams, function (err, rows, fields) {
+    if (err) throw err;
+    // console.log(rows);
+    res.send(rows);
+  });
+}); //api/getFavorites
 
 //starting server
 app.listen(PORT, IP, function () {
--- public/js/functions.js
+++ public/js/functions.js
@@ -34,4 +34,48 @@ $(document).ready(function () {
       success: function (data, status) {},
     }); //ajax
   } //updateFavorite
+
+  //When clicking on any keyword link,
+  //all corresponding images are displayed
+  $(".keywordLink").on("click", function () {
+    let keyword = $(this).html().trim();
+    $("a").removeClass("active");
+    $(this).addClass("active");
+    $("#keywordSelected").val(keyword);
+    $.ajax({
+      method: "get",
+      url: "/api/getFavorites",
+      data: {
+        keyword: keyword,
+      },
+      success: function (data, status) {
+        $("#favorites").html("");
+        let htmlString = "";
+        $("#favorites").css("visibility", "visible");
+        data.forEach(function (row) {
+          htmlString +=
+            "<img class='image' src='" +
+            row.imageURL +
+            "' width='200' height='200'>";
+          htmlString +=
+            "<img class='favoriteIcon' src='img/favorite_on.png' width='20'>";
+        });
+
+        $("#favorites").append(htmlString);
+      },
+    }); //ajax
+  }); //keywordLink
+
+  //Event for dynamic content generated when clicking on a keyword
+  $("#favorites").on("click", ".favoriteIcon", function () {
+    let favorite = $(this).prev().attr("src");
+
+    if ($(this).attr("src") == "img/favorite.png") {
+      $(this).attr("src", "img/favorite_on.png");
+      updateFavorite("add", favorite, $("#keywordSelected").val());
+    } else {
+      $(this).attr("src", "img/favorite.png");
+      updateFavorite("delete", favorite);
+    }
+  }); //.favoriteIcon
 }); //ready
